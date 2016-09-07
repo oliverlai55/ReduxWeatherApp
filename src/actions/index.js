e6d75e10c1e93f7a4173d49f9362907142ac8606
oliverlai55 <oliverlai55@gmail.com>
2016-09-07 00:46:07 -0400

    Install axios for ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
 const API_KEY = '0b66f0f8e1b5e15a5b59581c421348aa';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER ='FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
