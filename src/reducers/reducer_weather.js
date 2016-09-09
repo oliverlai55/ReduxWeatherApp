4dccdacd2f1c325b109d8f69a31e383e327fbea6
oliverlai55 <oliverlai55@gmail.com>
2016-09-09 03:09:16 -0400

    Return data as state in reducers, but no mutate state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,13 @@
-export default function(state = null, action) {
-  
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      //don't mutate state, make sure to return new state
+      return [ action.payload.data, ...state ];
+      //ES6, make new array, put action.payload.data in it.
+      // take all the things and add in state.
+      //return state.concat([ action.payload.data ]);
+  }
   return state;
 }
