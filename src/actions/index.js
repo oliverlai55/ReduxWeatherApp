c0cec1484093cc09b86f278dcb1db4c02ff9336f
oliverlai55 <oliverlai55@gmail.com>
2017-08-06 23:43:22 -0400

    Add notes on reducers, middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 // Using ES6 template strings, with `` and $
 
 export const FETCH_WEATHER ='FETCH_WEATHER';
-
+// keep the action type consistent between creators and reducers
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 // Place to put middleware
-
+// Middleware can do tons of tasks to manipulage or log or stop Actions BEFORE you reach the reducer
+// Redux-promise helps with handle ajax request in the app
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
